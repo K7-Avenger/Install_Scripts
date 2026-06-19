d5c6cdc5197657837a34dfe8763bc33b7f394175
K7-Avenger <48218837+K7-Avenger@users.noreply.github.com>
2026-06-19 14:17:18 -0500

    Update ai-prep.sh

--- ai-prep.sh
+++ ai-prep.sh
@@ -67,11 +67,11 @@ validateFileChecksums(){
 }
 
 install-ai-dependancies(){
-  sudo apt install LM-Studio-0.4.16-2-x64.deb -y
-  bash install.sh
+  sudo apt install $INSTALL_DIR/LM-Studio-0.4.16-2-x64.deb -y
+  bash $INSTALL_DIR/install.sh
   #curl -fsSL https://lmstudio.ai/install.sh | bash #<--llmster is LM Studio's headless daemon for servers, cloud instances, and CI. May not be needed with full install.
   #curl -fsSL https://lmstudio.ai/download/latest/linux/x64?format=deb
-  curl -fsSL https://hermes-agent-nousresearch.com/install.sh | bash #<--Convert to wget
+  #curl -fsSL https://hermes-agent-nousresearch.com/install.sh | bash #<--Convert to wget
   #hermes setup #
 }
 
